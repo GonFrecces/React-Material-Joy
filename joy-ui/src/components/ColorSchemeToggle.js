f0f9fc7717fe262ee99b8a56739e6909aac3ecd4
GonFrecces <54601577+GonFrecces@users.noreply.github.com>
2024-11-25 09:26:16 -0300

    Add modal login

---
+++ joy-ui/src/components/ColorSchemeToggle.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { useColorScheme } from '@mui/joy/styles';
+import IconButton from '@mui/joy/IconButton';
+
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+const ColorSchemeToggle = (props) => {
+
+    const { onClick, sx, ...other } = props;
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+
+        setMounted(true);
+
+    }, []);
+
+    if (!mounted) {
+        return (
+            <IconButton
+                size="sm"
+                variant="outlined"
+                color="neutral"
+                {...other}
+                sx={sx}
+                disabled
+            />
+        );
+    }
+    return (
+        <IconButton
+            data-screenshot="toggle-mode"
+            size="sm"
+            variant="outlined"
+            color="neutral"
+            {...other}
+            onClick={(event) => {
+                if (mode === 'light') {
+                    setMode('dark');
+                } else {
+                    setMode('light');
+                }
+                onClick?.(event);
+            }}
+            sx={[
+                mode === 'dark'
+                    ? { '& > *:first-of-type': { display: 'none' } }
+                    : { '& > *:first-of-TYPE': { display: 'initial' } },
+                mode === 'light'
+                    ? { '& > *:last-child': { display: 'none' } }
+                    : { '& > *:last-child': { display: 'initial' } },
+                ...(Array.isArray(sx) ? sx : [sx]),
+            ]}
+        >
+            <DarkModeRoundedIcon />
+            <LightModeIcon />
+        </IconButton>
+    );
+}
+
+export default ColorSchemeToggle;
---
+++ joy-ui/src/components/modal/login.js
@@ -0,0 +1,60 @@
+import React, { useRef } from 'react';
+import { Transition } from 'react-transition-group';
+import { Modal, ModalDialog, ModalClose, DialogTitle, DialogContent, Typography, Sheet, Button } from '@mui/joy';
+
+const FadeModalDialog = (props) => {
+
+    const { open, setOpen } = props ? props : {};
+    const modalRef = useRef(null);
+
+    return (
+        <Transition in={open} timeout={400} nodeRef={modalRef}>
+            {(state) => (
+                <Modal
+                    keepMounted
+                    open={!['exited', 'exiting'].includes(state)}
+                    onClose={() => setOpen(false)}
+                    
+                    ref={modalRef}
+                    slotProps={{
+                        backdrop: {
+                            sx: {
+                                opacity: 0,
+                                backdropFilter: 'none',
+                                transition: `opacity 400ms, backdrop-filter 400ms`,
+                                ...{
+                                    entering: { opacity: 1, backdropFilter: 'blur(8px)' },
+                                    entered: { opacity: 1, backdropFilter: 'blur(8px)' },
+                                }[state],
+                            },
+                        },
+                    }}
+                    sx={[
+                        state === 'exited'
+                            ? { visibility: 'hidden' }
+                            : { visibility: 'visible' },
+                    ]}
+                >
+                    
+                    <ModalDialog
+                        sx={{
+                            opacity: 0,
+                            transition: `opacity 300ms`,
+                            ...{
+                                entering: { opacity: 1 },
+                                entered: { opacity: 1 },
+                            }[state],
+                        }}
+                    >
+                        <DialogTitle>Transition modal</DialogTitle>
+                        <DialogContent>
+                            Using `react-transition-group` to create a fade animation.
+                        </DialogContent>
+                    </ModalDialog>
+                </Modal>
+            )}
+        </Transition>
+    );
+}
+
+export default FadeModalDialog;
